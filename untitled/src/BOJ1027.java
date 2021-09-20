a1a239b8b5c975e3d56ab3e0c303ef646398e974
jhlee11-alticast <jh.lee11@alticast.com>
2021-09-20 19:20:10 +0900

    BOJ1027 rollback

--- untitled/src/BOJ1027.java
+++ untitled/src/BOJ1027.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -11,11 +10,27 @@ public class BOJ1027 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int count = Integer.parseInt(br.readLine());
-        List<Integer> myList = new ArrayList<>();
-        for(int i = count; i > 0; i--) {
-            int m = Integer.parseInt(br.readLine());
-            System.out.println(m);
+        int n = Integer.parseInt(br.readLine());
+        int[] nArr = Arrays.stream(br.readLine().split(" ")).map(Integer::parseInt).mapToInt(i -> i).toArray();
+        int m = Integer.parseInt(br.readLine());
+        List<Integer> mList = Arrays.stream(br.readLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+
+        HashMap<Integer, Integer> myHashMap = new HashMap<>();
+        for(int e: mList) {
+            myHashMap.put(e, 0);
         }
+
+        for(int e: nArr) {
+            Integer valTaken = myHashMap.get(e);
+            if(valTaken != null) {
+                myHashMap.put(e, valTaken + 1);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            sb.append(myHashMap.get(mList.get(i)).toString());
+            sb.append(" ");
+        }
+        System.out.println(sb.toString().trim());
     }
 }
