eb49ec5320dea9fe6a5a1355b4786be093289e7a
biryong3419 <macknick@naver.com>
2021-10-01 18:22:37 +0900

    ExtraLongFactorials clear

---
+++ untitled/src/ExtraLongFactorials.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result33 {
+
+    /*
+     * Complete the 'extraLongFactorials' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void extraLongFactorials(int n) {
+        // Write your code here
+        BigInteger bi = BigInteger.ONE;
+        for(int i = 2; i <= n; i ++){
+            bi = bi.multiply(BigInteger.valueOf(i));
+        }
+        System.out.println(bi);
+    }
+
+}
+
+class Solution33 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result33.extraLongFactorials(n);
+
+        bufferedReader.close();
+    }
+}
